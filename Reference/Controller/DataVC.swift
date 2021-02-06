f7c247e70116ccc8fb746eb6b41a01f427b8ccf9
Alexey Davletshin <naylrush@gmail.com>
2021-02-06 21:48:18 +0300

    Rename VCs

--- Reference/Controller/DataVC.swift
+++ Reference/Controller/DataVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DataVC.swift
 //  Reference
 //
 //  Created by Alexey Davletshin on 05.02.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DataVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
